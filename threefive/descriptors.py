282303fff9d1a623fa7d3cbe21a5015a68798baa
Adrian of Doom <52701496+futzu@users.noreply.github.com>
2024-11-25 19:29:21 -0500

    massive update on SegmentationDescriptor.from_xml()

--- threefive/descriptors.py
+++ threefive/descriptors.py
@@ -560,18 +560,20 @@ class SegmentationDescriptor(SpliceDescriptor):
         """
         Load a SegmentationDescriptor from XML
         """
-        self.load(stuff["SegmentationDescriptor"])
+        ssd = stuff["SegmentationDescriptor"]
+        self.load(ssd)
         self.segmentation_event_id_compliance_indicator = True
         self.program_segmentation_flag = True
         self.segmentation_duration_flag = False
-        if "segmentationDuration" in stuff["SegmentationDescriptor"]:
+        if "segmentationDuration" in ssd:
             self.segmentation_duration_flag = True
         self.delivery_not_restricted_flag = True
         if "DeliveryRestrictions" in stuff:
+            sdr = stuff["DeliveryRestrictions"]
             self.delivery_not_restricted_flag = False
-            self.load(stuff["DeliveryRestrictions"])
-            self.device_restrictions = table20[self.device_restrictions]
-        self.segmentation_event_id = hex(self.segmentation_event_id)
+            self.load(sdr)
+            self.device_restrictions = table20[sdr["device_restrictions"]]
+        self.segmentation_event_id = hex(ssd["segmentation_event_id"])
         if self.segmentation_type_id in table22:
             self.segmentation_message = table22[self.segmentation_type_id]
         self._upid_from_xml(stuff)
